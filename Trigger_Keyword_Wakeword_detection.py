3adb81bad2353c231fb3a04e713cd8044fb287f4
Develop_It <developit19@gmail.com>
2019-05-10 17:22:50 +0530

    Create Trigger_Keyword_Wakeword_detection.py

---
+++ Trigger_Keyword_Wakeword_detection.py
@@ -0,0 +1,13 @@
+#Import Dependencies
+import numpy as np
+from pydub import AudioSegment
+import random
+import sys
+import io
+import os
+import glob
+import IPython
+from td_utils import *
+import matplotlib.pyplot as plt
+%matplotlib inline
+
